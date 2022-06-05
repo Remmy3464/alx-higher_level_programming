5ce05cf4ec3e5256f86348467766a562b020b5d5
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-06-05 10:35:52 +0100

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    a, operand, b = int(argv[1]), argv[2], int(argv[3])
+    arith = {'+': add, '-': sub, '*': mul, '/': div}
+    if operand not in arith:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print('{:d} {} {:d} = {:d}'.format(a, operand, b, arith[operand](a, b)))
