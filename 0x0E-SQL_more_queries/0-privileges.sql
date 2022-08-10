bd3f5d1a09da5fce75fddd0693e60ac727f85da8
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-08-10 21:12:47 +0100

    more sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- list permissions for users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'remilekun001@';
+SHOW GRANTS FOR 'user_0d_2'@'olaremioo002@';
--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,4 +4,4 @@ FROM tv_genres JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY tv_genres.name
-ORDER BY `number_shows` DESC;
+ORDER BY `number_of_shows` DESC;
