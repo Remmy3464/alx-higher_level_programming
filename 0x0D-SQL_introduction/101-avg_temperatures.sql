cb75d095cb1035538e6e8154dc2c9d43cd7a87f4
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-08-10 02:08:15 +0100

    MySQL

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,3 @@
--- Calculates average of a column.
-SELECT city, AVG('value') AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- Import in hbtn_0c_0 database this table dump
+-- Displays cities avg temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
