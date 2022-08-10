79066a5360233d7029f9ac86b97a0bca04c700a6
Adesanmi Aderemi <adesanmiaderemi@hotmail.com>
2022-08-10 21:19:03 +0100

    more sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,6 @@
--- select rows from different tables
+-- lists all shows contained in hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY title, genre_id ASC;
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id 
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
