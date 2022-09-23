056b7fd41a7fe4a570c3d3fddc4c030655dd1e20
Remmy3464 <adesanmiaderemi@hotmail.com>
2022-09-23 23:05:20 +0100

    Python - Network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status"""
+"""Fetches https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
 
     print('Body response:')
     print('\t- type: {}'.format(type(r.text)))
