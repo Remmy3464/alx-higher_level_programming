81c9fd4b669fe0e3a00c6ba3b82a372d8d3a6058
Remmy3464 <adesanmiaderemi@hotmail.com>
2022-10-18 23:46:16 +0100

    JavaScript

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/:id' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
